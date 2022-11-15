9eca4dae6546603057d25a2551bbb53984c813f5
llybood <llybood@yahoo.com>
2022-11-15 21:15:38 +0800

    增加脚本可以获取多个metric数据功能

--- script/test.sh
+++ script/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #this is a test
-echo '{"instances": [{"instance": "test1","type": "business","value":"1000"},{"instance": "test2","type": "business", "value":"2000"}],"metric": "test","description": "this is a test"}'
+echo '[{"instances": [{"instance": "test1","type": "business","value":"1000"},{"instance": "test2","type": "business", "value":"2000"}],"metric": "test1","description": "this is test1"},{"instances": [{"instance": "test3","type": "module","value":"3000"},{"instance": "test4","type": "module", "value":"4000"}],"metric": "test2","description": "this is test2"}]'
